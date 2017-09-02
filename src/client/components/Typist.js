99d1e38e3e3c4f8e2cfea18714d58e5a47b9202c
Dragos Nedelcu <dragos.nedelcu@lokalleads.de>
2017-09-02 19:15:22 +0200

    typing works

--- src/client/components/Typist.js
+++ src/client/components/Typist.js
@@ -1,23 +1,35 @@
 import React, {Component} from 'react'
 import Typist from 'react-typist'
 
-const typedtext = [
+const typedText = [
   "instant quotes for your new bathroom",
   "advices realted to your bathroom renovation"
 ]
 
 
 export default class MyComponent extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      textIndex: 0
+    }
+
+    this.onTypingDone = this.onTypingDone.bind(this)
+  }
+
+  onTypingDone() {
+    console.log(this.state.textIndex, typedText.length - 1)
+    if(this.state.textIndex <= typedText.length) {
+      this.setState({textIndex: this.state.textIndex + 1})
+    }else if(this.state.textIndex > typedText.length - 1) {
+      this.setState({textIndex: 0})
+    }
+  }
+
   render() {
     return(
-      <Typist avgTypingDelay={40} onTypingDone={this.next}>
-        {
-        typedtext.map((item) => {
-               <span key={item}>
-                   <h1>{item}</h1>
-               </span>
-          })
-      }
+      <Typist key={this.state.textIndex} avgTypingDelay={90} onTypingDone={this.onTypingDone} startDelay={1000}>
+        {typedText[this.state.textIndex]}
       </Typist>
     )
   }
