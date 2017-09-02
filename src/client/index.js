76ea33659244907aaf285084e983de6d54ef1a4d
Bogdan Nedelcu <bogdan.nedelcu@lokalleads.de>
2017-09-02 16:46:26 +0200

    dev redux

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,24 @@
 import {render} from 'react-dom'
 import React from 'react'
 import {ThemeProvider} from 'styled-components'
+import {createStore, applyMiddleware, combineReducers} from 'redux'
+import {Provider} from "react-redux"
+import {composeWithDevTools} from 'redux-devtools-extension'
 
 import App from "./components/App"
+import chatReducer from "./reducers/chat"
 
+const rootReducer = combineReducers({
+  chat: chatReducer,
+})
+
+let store = createStore(rootReducer, composeWithDevTools(
+  enhancer,
+  applyMiddleware(
+    thunk.withExtraArgument({client,routeQueryMap}),
+    middleware,
+  ),
+))
 
 const theme = {
   brandColor: "#3498db",
@@ -14,7 +29,9 @@ const theme = {
 const renderApp = () => {
   render(
     <ThemeProvider theme={theme}>
-      <App/>
+      <Provider store={store}>
+        <App/>
+      </Provider>
     </ThemeProvider>
     , document.getElementById("app"))
 }
