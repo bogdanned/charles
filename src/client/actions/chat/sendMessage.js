2de9e8900a390f4647c964fb5b60b737126dcd93
Bogdan Nedelcu <bogdan.nedelcu@lokalleads.de>
2017-09-02 19:47:04 +0200

    async actions

--- src/client/actions/chat/sendMessage.js
+++ src/client/actions/chat/sendMessage.js
@@ -1,3 +1,11 @@
+
+
+async function fetchReply(message){
+  const reply = await fetch()
+  return reply
+}
+
+
 export default ({inputChat}) => {
   return {type: "SEND_MESSAGE", payload: {inputChat}}
 }
