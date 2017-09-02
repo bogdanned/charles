066b827eaa741282bcb94b5397ab5f3513b5c666
Dragos Nedelcu <dragos.nedelcu@lokalleads.de>
2017-09-02 19:26:23 +0200

    goood typed

--- src/client/components/Typist.js
+++ src/client/components/Typist.js
@@ -3,7 +3,7 @@ import Typist from 'react-typist'
 
 const typedText = [
   "instant quotes for your new bathroom",
-  "advices realted to your bathroom renovation"
+  "advices related to your bathroom renovation"
 ]
 
 
@@ -13,22 +13,20 @@ export default class MyComponent extends Component {
     this.state = {
       textIndex: 0
     }
-
     this.onTypingDone = this.onTypingDone.bind(this)
   }
 
   onTypingDone() {
-    console.log(this.state.textIndex, typedText.length - 1)
     if(this.state.textIndex <= typedText.length) {
       this.setState({textIndex: this.state.textIndex + 1})
     }else if(this.state.textIndex > typedText.length - 1) {
       this.setState({textIndex: 0})
-    }
   }
+}
 
   render() {
     return(
-      <Typist key={this.state.textIndex} avgTypingDelay={90} onTypingDone={this.onTypingDone} startDelay={1000}>
+      <Typist key={this.state.textIndex} avgTypingDelay={90} onTypingDone={() => setTimeout(this.onTypingDone, 1000)} startDelay={1000}>
         {typedText[this.state.textIndex]}
       </Typist>
     )
