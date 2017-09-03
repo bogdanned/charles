80c1b06ffed1959600905b318dc9f4e436e9ffc7
Bogdan Nedelcu <bogdan.nedelcu@lokalleads.de>
2017-09-03 13:38:57 +0200

    cors

--- src/client/actions/chat/fetchReplyMessage.js
+++ src/client/actions/chat/fetchReplyMessage.js
@@ -15,7 +15,7 @@ export default (message) => {
     // In this case, we return a promise to wait for.
     // This is not required by thunk middleware, but it is convenient for us.
 
-    const reply = await fetch('http://127.0.0.1:3000/sendMessage', {
+    const reply = await fetch('/sendMessage', {
       method : 'POST',
       headers: {
         'Content-Type': 'application/json'
