18a204dfea80298259893923fe760e6550331338
Dragos Nedelcu <dragos.nedelcu@lokalleads.de>
2017-09-03 17:14:49 +0200

    styled footer

--- src/client/components/Footer.js
+++ src/client/components/Footer.js
@@ -29,10 +29,10 @@ const FooterLink = styled.div`
 export default() => (
   <Footer>
     <FooterContent>
-      <FooterLink><h3>Lohnbot.ai</h3></FooterLink>
-      <FooterLink><h3>How it works</h3></FooterLink>
-      <FooterLink><h3>Pricing</h3></FooterLink>
-      <FooterLink><h3>Contact</h3></FooterLink>
+      <FooterLink><h5><strong>Lohnbot.ai</strong></h5></FooterLink>
+      <FooterLink><h5><strong>How it works</strong></h5></FooterLink>
+      <FooterLink><h3><strong>Pricing</strong></h3></FooterLink>
+      <FooterLink><h3><strong>Contact</strong></h3></FooterLink>
     </FooterContent>
   </Footer>
 )
