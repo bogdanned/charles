557bb3b97731566d0e0ff7911d613afd252621c8
Dragos Nedelcu <dragos.nedelcu@lokalleads.de>
2017-09-03 12:54:45 +0200

    further styles

--- src/client/components/Contact.js
+++ src/client/components/Contact.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import {compose, withHandlers} from 'recompose'
 
-export const StyledH1 = styled.h1`
+const StyledH1 = styled.h1`
   font-size: 40px;
 `
 
-export const DetailText = styled.h3`
+const DetailText = styled.h3`
   font-weight: lighter;
   color: ${props => props.theme.secondaryFontColor};
 `
@@ -22,14 +23,44 @@ const Root = styled.div`
   color: ${props => props.theme.terciaryFontColor};
 `
 
-export default() => (
+const StyledInput = styled.input`
+  padding: 0.5rem;
+  border: none;
+  border-radius: 0.25rem;
+`
+
+const FormBox = styled.div`
+  margin: 2rem;
+`
+
+const SendButton = styled.button`
+  background-color: ${props => props.theme.brandColor};
+  color: ${props => props.theme.secondaryColor};
+  padding: 0.5rem;
+  border-radius: 0.25rem;
+  border: none;
+  margin: 0.25rem;
+`
+
+let Contact = ({onSubmit}) => (
   <Root>
     <StyledH1><strong>lohnbot.ai</strong></StyledH1>
     <StyledH1>Your personal bathroom planner</StyledH1>
     <DetailText>Juts tell me your bathroom doubts and concerns</DetailText>
-    <form>
-      <input type ="text" placeholder="Enter your email" name="email"/>
-      <button type="submit">Sign up</button>
-    </form>
+    <FormBox>
+      <form>
+        <StyledInput type ="text" placeholder="Enter your email" name="email"/>
+        <SendButton type="submit" onClick={(e) => onSubmit(e)}>Sign up</SendButton>
+      </form>
+    </FormBox>
   </Root>
 )
+
+export default compose(
+  withHandlers({
+    onSubmit: () => (e, value) => {
+      e.preventDefault()
+      console.log(value)
+    }
+  })
+)(Contact)
