7f5ae1d1f5c604d4d129332b55469578d9fad533
Bogdan Mihai <bogdanned32@gmail.com>
2017-10-05 21:05:45 +0200

    flask docker file

---
+++ bin/build-and-deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+unset REPOSITORY
+
+export REPOSITORY=""
+export BASE_DIR=`pwd`
+
+docker swarm leave --force || /usr/bin/true
+docker swarm init
+
+docker-compose -f docker-compose.yml down
+
+$(dirname $0)/build.sh
+
+docker-compose -f docker-compose.yml up
