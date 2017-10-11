ee39bbda742f447e05c0aad3a14ef0a659fd09e8
Bogdan Mihai <bogdanned32@gmail.com>
2017-10-11 22:27:45 +0200

    ngrok setup

--- app_platon/app/slack.py
+++ app_platon/app/slack.py
@@ -1,15 +1,16 @@
 import os
 from slackclient import SlackClient
 
-SLACK_TOKEN = 'xoxp-67269840914-67272102882-255209948774-c37a10ea18fb0a88d6aea5211c272047'
-slack_client = SlackClient(SLACK_TOKEN)
+SLACK_TOKEN = 'xoxb-254478435541-qnywnHba59JnAHJfeXQyBRVv'
+sc = SlackClient(SLACK_TOKEN)
 
+sc.api_call(
+  "channels.join",
+  channel="#general"
+)
 
-# Print Tests
-print(slack_client.api_call("api.test"))
-print(slack_client.api_call("auth.test"))
-
-# {u'args': {u'token': u'xoxp-67269840914-67272102882-255209948774-c37a10ea18fb0a88d6aea5211c272047'}, u'ok': True}
-
-
-# Api
+sc.api_call(
+  "chat.postMessage",
+  channel="#general",
+  text="Hello from Python! :tada:",
+)
